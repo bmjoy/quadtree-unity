2ea6a5b8438be56d9b3d96c227ddda1bf60d27ce
zhaolihang <625335512@qq.com>
2021-06-20 13:31:08 +0800

    文件重命名

---
+++ Assets/Scripts/QuadTreeTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadTreeTest : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+    }
+}
