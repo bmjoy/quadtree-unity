8d7df3ef65f40fe9a8b750198fea0798d02d0b96
zhaolihang <625335512@qq.com>
2021-06-23 22:50:47 +0800

    新场景

---
+++ Assets/Scripts/QuadTreeTestDynamic.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Diagnostics = System.Diagnostics;
+using UnityEngine;
+using UnityEditor;
+using Random = UnityEngine.Random;
+using Sirenix.OdinInspector;
+using RectInfo = QuadTree.RectInfo;
+using Rect = MyRect;
+
+[ExecuteInEditMode]
+[DisallowMultipleComponent]
+public class QuadTreeTestDynamic : QuadTreeTestBase
+{
+    protected override void checkMouse()
+    {
+        if (IsMouseMove && hero != null)
+        {
+            var pos = MouseMoveWorldPos;
+            {
+                hero.rect.x = pos.x - hero.rect.width / 2;
+                hero.rect.y = pos.y - hero.rect.height / 2;
+            }
+        }
+        if (IsMouseClick)
+        {
+            var pos = MouseClickWorldPos;
+            addObj(new Vector2(pos.x, pos.y));
+        }
+        if (IsMouseMove)
+        {
+            checkQuery();
+        }
+    }
+}
